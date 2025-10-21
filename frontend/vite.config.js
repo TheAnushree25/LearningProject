d616151c3fd2d3dde578831b56a96a3af6f31f97
TheAnushree25 <theanushree25@gmail.com>
2025-10-21 15:27:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:4400'
+      '/api': "https://learningproject-backend.onrender.com"
+
     }
   },
   plugins: [react()],
